b29e87133d61f73f0a6aa87f8f3e1411c69ed1f3
SG150901 <silviaglas159@gmail.com>
2025-01-13 16:45:29 +0100

    updated file  (now 6 beams, ...)

--- scann_pattern_numerical.py
+++ scann_pattern_numerical.py
@@ -1,17 +1,10 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import tqdm
 
-def refract_beam(v, n, nl, nr):
-    # get the direction vector of the beam
-    b = v
-    # get the normal vector of the plane
-    n = n
-    # get the refractive index of the left medium
-    nl = nl
-    # get the refractive index of the right medium
-    nr = nr
 
+def refract_beam(b, n, nl, nr):
     # calculate the angle of incidence
     cos_delta_i = np.dot(b, -n)  # b and n are normalized
     # calculate the angle of refraction
@@ -20,6 +13,7 @@ def refract_beam(v, n, nl, nr):
     b = nl / nr * b + (nl / nr * cos_delta_i - cos_delta_r) * n
 
     return b
+
 
 def intersect(g, e):
     # get the point and the direction vector on the line g
@@ -35,6 +29,8 @@ def intersect(g, e):
 
     return P
 
+
+# noinspection PyUnreachableCode
 def rot_surfaces(t, n, omega, theta0):
     # calculate the rotation angle
     theta = omega * t + theta0
@@ -47,38 +43,43 @@ def rot_surfaces(t, n, omega, theta0):
 
     return n
 
+
 def main():
     # define all the parameters
-    # integration time [s]
-    t = 0.00
-    T = 0.01
     # refractive index of the prisms
     n1 = 1.5
     n2 = 1.5
     n3 = 1.5
     nAir = 1.0
     # angle of prisms (in radians); calculated from k (ratio of deviation angles)
-    alpha1_deg = 18.8
+    alpha1_deg = 15.0
+    # alpha1_deg = 17.9243
     alpha1 = np.deg2rad(alpha1_deg)
-    k1 = 1.0
-    k2 = 0.7
+    # k1 = 0.775
+    k1 = 0.75
+    k2 = 0.2
     alpha2 = k1 * alpha1 * (n1 - 1) / (n2 - 1)
     alpha3 = k2 * alpha1 * (n1 - 1) / (n3 - 1)
     # rotation velocity of the prisms (in radians per  second) and initial rotation angle (in radians)
+    # theta01 = np.pi/2
     theta01 = 0.0
+    # theta02 = np.pi/2
     theta02 = 0.0
+    # theta03 = np.pi/2
     theta03 = 0.0
     omega1_rpm = 4000
     omega1 = omega1_rpm * 2 * np.pi / 60  # rad per sec
-    M1 = -2.0
-    M2 = 0.15
+    # M1 = -4.81
+    M1 = -1.0
+    # M2 = 1.0
+    M2 = -3.0
     omega2 = omega1 * M1
     omega3 = omega1 * M2
 
     # definition of the prism surfaces
     # prism 1 (12)
-    d1 = 2.0  # mm
-    R1 = 20.0  # mm
+    d1 = 15.0  # mm
+    R1 = 16.0  # mm
     # points on the prism surfaces of rotation axis
     P11 = np.array([0, 0, 0])
     z12 = d1 + R1 * np.tan(alpha1)
@@ -86,113 +87,128 @@ def main():
     # normal vectors to the prism surfaces
     n11 = np.array([0, 0, 1])
     n12 = np.array([0, np.sin(alpha1), np.cos(alpha1)])
+    n12_0 = n12
     # air between prism 1 and prism 2
-    dair1 = 10  # mm
+    dair1 = 2.4  # mm
 
     # prism 2 (21)
-    d2 = 2.0  # mm
-    R2 = 20.0  # mm
+    d2 = 15.0  # mm
+    R2 = 16.0  # mm
     # points on the prism surfaces of rotation axis
     P21 = np.array([0, 0, z12 + dair1])
-    z22 = z12  + dair1 + d2 + R2 * np.tan(alpha2)
+    z22 = z12 + dair1 + d2 + R2 * np.tan(alpha2)
     P22 = np.array([0, 0, z22])
     # normal vectors to the prism surfaces
     n21 = np.array([0, -np.sin(alpha2), np.cos(alpha2)])
+    n21_0 = n21
     n22 = np.array([0, 0, 1])
     # air between prism 2 and prism 3
-    dair2 = 600  # mm
+    dair2 = 2.4  # mm
 
     # prism 3 (21)
-    d3 = 2.0  # mm
-    R3 = 20.0  # mm
+    d3 = 15.0  # mm
+    R3 = 16.0  # mm
     # points on the prism surfaces of rotation axis
     P31 = np.array([0, 0, z22 + dair2])
     z32 = z22 + dair2 + d3 + R3 * np.tan(alpha3)
     P32 = np.array([0, 0, z32])
     # normal vectors to the prism surfaces
     n31 = np.array([0, -np.sin(alpha3), np.cos(alpha3)])
+    n31_0 = n31
     n32 = np.array([0, 0, 1])
 
     # distance to observation plane
-    D = 1000  # mm
+    D = 10.00  # mm
     # observation plane
     P4 = np.array([0, 0, z32 + D])
+    # P4 = np.array([0, 0, z22 + D])
     n4 = np.array([0, 0, 1])
 
     # beam line
     P00 = np.array([0, 0, -1])
-    b00 = np.array([0, 0, 1])
+    # lims = 0.006962
+    lims = 0.006
+    b00s = [np.array([a, 0, 1])/np.linalg.norm(np.array([a, 0, 1])) for a
+            in np.linspace(-lims, lims, 6)
+            ]
+    ts = np.arange(0, 0.075*1, 1/240000)
 
     # list for the intersection point coordinates with the observation plane
-    PobsX = []
-    PobsY = []
-    PobsZ = []
+    PobsX = np.empty((ts.shape[0]*6,), dtype=float)
+    PobsY = np.empty((ts.shape[0]*6,), dtype=float)
+    PobsZ = np.empty((ts.shape[0]*6,), dtype=float)
 
     # loop over the integration time
-    for t in tqdm.tqdm(np.arange(0, 0.0003333, 1/200000000)):
-        # Refraction at surface 11
-        Pb11 = intersect((P00, b00), (P11, n11))
-        b11 = refract_beam(b00, n11, nAir, n1)
+    for tix, t in enumerate(tqdm.tqdm(ts)):
+        for bix, b00 in enumerate(b00s):
+            # Refraction at surface 11
+            Pb11 = intersect((P00, b00), (P11, n11))
+            b11 = refract_beam(b00, n11, nAir, n1)
 
-        # Refraction at surface 12
-        Pb12 = intersect((Pb11, b11), (P12, n12))
-        b12 = refract_beam(b11, n12, n1, nAir)
+            # Refraction at surface 12
+            Pb12 = intersect((Pb11, b11), (P12, n12))
+            b12 = refract_beam(b11, n12, n1, nAir)
 
-        # Refraction at surface 21
-        Pb21 = intersect((Pb12, b12), (P21, n21))
-        b21 = refract_beam(b12, n21, nAir, n2)
+            # Refraction at surface 21
+            Pb21 = intersect((Pb12, b12), (P21, n21))
+            b21 = refract_beam(b12, n21, nAir, n2)
 
-        # Refraction at surface 22
-        Pb22 = intersect((Pb21, b21), (P22, n22))
-        b22 = refract_beam(b21, n22, n2, nAir)
+            # Refraction at surface 22
+            Pb22 = intersect((Pb21, b21), (P22, n22))
+            b22 = refract_beam(b21, n22, n2, nAir)
 
-        # Refraction at surface 31
-        Pb31 = intersect((Pb22, b22), (P31, n31))
-        b31 = refract_beam(b22, n31, nAir, n3)
+            # Refraction at surface 31
+            Pb31 = intersect((Pb22, b22), (P31, n31))
+            b31 = refract_beam(b22, n31, nAir, n3)
+    
+            # Refraction at surface 32
+            Pb32 = intersect((Pb31, b31), (P32, n32))
+            b32 = refract_beam(b31, n32, n3, nAir)
 
-        # Refraction at surface 32
-        Pb32 = intersect((Pb31, b31), (P32, n32))
-        b32 = refract_beam(b31, n32, n3, nAir)
-
-        # Refraction at observation plane
-        Pobs = intersect((Pb32, b32), (P4, n4))
-        # Pobs = intersect((Pb12, b12), (P4, n4))
-        # Pobs = intersect((Pb22, b22), (P4, n4))
-        # append the intersection point with the observation plane
-        PobsX.append(Pobs[0])
-        PobsY.append(Pobs[1])
-        PobsZ.append(Pobs[2])
-
-        """# plot the beam line
-        plt.plot([P00[2], Pb11[2], Pb12[2], Pb21[2], Pb22[2], Pb31[2], Pb32[2], Pobs[2]], [P00[1], Pb11[1], Pb12[1], Pb21[1], Pb22[1], Pb31[1], Pb32[1], Pobs[1]], 'o')
-        plt.axis('equal')
-        plt.show()"""
-
-        # increase the time
+            # Refraction at observation plane
+            Pobs = intersect((Pb32, b32), (P4, n4))
+            # Pobs = intersect((Pb12, b12), (P4, n4))
+            # Pobs = intersect((Pb22, b22), (P4, n4))
+            # append the intersection point with the observation plane
+            PobsX[tix*6+bix] = Pobs[0]
+            PobsY[tix*6+bix] = Pobs[1]
+            PobsZ[tix*6+bix] = Pobs[2]
 
         # rotate the normal vectors of the prism surfaces (only the angular surfaces(Nr.2); the others stay the same)
-        n12 = rot_surfaces(t, n12, omega1, theta01)
-        n21 = rot_surfaces(t, n21, omega2, theta02)
-        n31 = rot_surfaces(t, n31, omega3, theta03)
+        n12 = rot_surfaces(t, n12_0, omega1, theta01)
+        n21 = rot_surfaces(t, n21_0, omega2, theta02)
+        n31 = rot_surfaces(t, n31_0, omega3, theta03)
 
     # show intersection with observation plane as plot
-    plt.plot(PobsX, PobsY, 'o', markersize=0.1)
+    plt.figure(figsize=(10, 10), dpi=300)
+    marker = matplotlib.markers.MarkerStyle(marker='o')
+    marker.fillstyle = 'full'
+    phi = np.atan2(PobsX, D) * 180/np.pi
+    kappa = np.atan2(PobsY, np.sqrt(np.square(PobsX) + D**2)) * 180/np.pi
+    # plt.scatter(PobsX, PobsY, marker=marker, s=0.01, c=ts.repeat(6), cmap='viridis')
+    plt.scatter(phi, kappa, marker=marker, s=0.01, c=ts.repeat(6), cmap='viridis')
     plt.axis('equal')
+    plt.title(f'M1={M1} M2={M2} k1={k1} k2={k2} alpha1={alpha1_deg} omega1={omega1_rpm} '
+              f'd1={d1}\nd2={d2} d3={d3} R1={R1} R2={R2} R3={R3} dair1={dair1} dair2={dair2} D={D}')
+    # plt.title(f'M1={M1} k1={k1} alpha1={alpha1_deg} omega1={omega1_rpm}\n'
+    #           f'd1={d1} d2={d2} R1={R1} R2={R2} dair1={dair1} D={D}')
+    # plt.xlim(-4, 4)
+    # plt.ylim(-30, -40)
+    # plt.xlim(-2, 2)
+    # plt.ylim(-2, 2)
     plt.show()
+
+    # print the maximal phi and kappa values
+    print(f'Maximal phi: {np.max(phi)}')
+    print(f'Maximal kappa: {np.max(kappa)}')
+    # get the phi values for kappa = 0 till 0.001
+    kappa_0 = 0
+    phi_0 = phi[np.where((kappa >= kappa_0) & (kappa <= kappa_0 + 0.001))]
+    print(f'Phi values for kappa = 0: {phi_0}')
+    # print the maximal and minimal phi value for kappa = 0
+    print(f'Maximal phi for kappa = 0: {np.max(phi_0)}')
+    print(f'Minimal phi for kappa = 0: {np.min(phi_0)}')
 
 
 if __name__ == '__main__':
     main()
-    """g = (np.array([1, 2, 3]), np.array([1, 1, 0]))
-    e = (np.array([1, 3, 3]), np.array([0.4, -0.8, 0]))
-    P = intersect(g, e)
-    print(P)
-    plt.plot(g[0][0], g[0][1], 'o')
-    plt.plot(e[0][0], e[0][1], 'o')
-    plt.plot([g[0][0], g[0][0]+g[1][0]], [g[0][1], g[0][1] + g[1][1]], 'b--')
-    plt.plot([e[0][0], e[0][0]+e[1][0]], [e[0][1], e[0][1] + e[1][1]], 'r--')
-    plt.plot(P[0], P[1], 'o')
-    plt.axis('equal')
-    plt.show()"""
-
-
